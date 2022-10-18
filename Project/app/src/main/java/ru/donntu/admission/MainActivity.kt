b03841332bf0228450eccf8b7a0e0d03c6d61a7c
Keronon S <59052298+Keronon@users.noreply.github.com>
2022-10-19 00:14:51 +0300

    19.10.22 Upload and Download realized

--- Project/app/src/main/java/ru/donntu/admission/MainActivity.kt
+++ Project/app/src/main/java/ru/donntu/admission/MainActivity.kt
@@ -1,32 +1,58 @@
 package ru.donntu.admission
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.DownloadManager
+import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.view.KeyEvent
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.os.Environment
+import android.webkit.*
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import java.io.File
+import java.net.URI
 
 
 class MainActivity : AppCompatActivity()
 {
     private lateinit var webView: WebView
 
+    var filePath: ValueCallback<Array<Uri>>? = null
+
+    @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val backButton : FloatingActionButton = findViewById(R.id.BTN_back)
+        val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+
+        val backButton: FloatingActionButton = findViewById(R.id.BTN_back)
         backButton.setOnClickListener { if (webView.canGoBack()) webView.goBack() }
         backButton.setOnLongClickListener { webView.reload(); return@setOnLongClickListener true; }
 
         webView = findViewById(R.id.webView)
+        webView.setOnLongClickListener {
+            val fileName = File(URI(webView.url).path).name
+            if (fileName.contains("."))
+                Snackbar.make(webView, "Save As", Snackbar.LENGTH_LONG)
+                        .setAction("Save") {
+                            val request = DownloadManager.Request(Uri.parse(webView.url))
+                                .setTitle(fileName)
+                                .setDescription("Downloading...")
+                                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
+                                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                                .setAllowedOverMetered(true)
+                            dm.enqueue(request) }.show()
+            return@setOnLongClickListener true
+        }
+
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = object : WebViewClient()
         {
@@ -36,19 +62,11 @@ class MainActivity : AppCompatActivity()
                 if (url != null) view?.loadUrl(url)
                 return true
             }
-
             override fun shouldOverrideUrlLoading(view: WebView,
                                                   request: WebResourceRequest): Boolean
             {
                 view.loadUrl(request.url.toString())
                 return true
-            }
-
-            override fun onPageFinished(view: WebView?, url: String?)
-            {
-                super.onPageFinished(view, url)
-                if (view != null)
-                    Snackbar.make(view, "Страница загружена!", Snackbar.LENGTH_SHORT).show()
             }
 
             override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?)
@@ -57,12 +75,74 @@ class MainActivity : AppCompatActivity()
                 if (view != null)
                     Snackbar.make(view, "Начата загрузка страницы", Snackbar.LENGTH_SHORT).show()
             }
-
-            override fun onUnhandledKeyEvent(view: WebView?, event: KeyEvent?)
+            override fun onPageFinished(view: WebView?, url: String?)
             {
-                super.onUnhandledKeyEvent(view, event)
+                super.onPageFinished(view, url)
+                if (view != null)
+                    Snackbar.make(view, "Страница загружена!", Snackbar.LENGTH_SHORT).show()
             }
         }
-        webView.loadUrl("http://pk2022.donntu.ru/")
+
+        webView.setDownloadListener { url, _, contentDisposition, mimetype, _ -> // userAgent ... contentLength
+            val fileName = URLUtil.guessFileName(url, contentDisposition, mimetype)
+            val request = DownloadManager.Request(Uri.parse(url))
+                .setTitle(fileName)
+                .setDescription("Downloading...")
+                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype))
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                .setAllowedOverMetered(true)
+
+            dm.enqueue(request)
+
+            //To notify the Client that the file is being downloaded
+            Snackbar.make(webView, "Downloading File", Snackbar.LENGTH_SHORT).show()
+        }
+
+        webView.webChromeClient = object : WebChromeClient()
+        {
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean
+            {
+                filePath = filePathCallback
+
+                val contentIntent = Intent(Intent.ACTION_GET_CONTENT)
+                contentIntent.type = "*/*"
+                contentIntent.addCategory(Intent.CATEGORY_OPENABLE)
+
+                startActivityForResult(contentIntent, 1)
+                return true
+            }
+        }
+
+        val targetUrl = "http://pk2022.donntu.ru/"
+        webView.loadUrl(targetUrl)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack()
+        else super.onBackPressed()
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
+    {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_CANCELED)
+        {
+            filePath?.onReceiveValue(null)
+            return
+        }
+        else if (resultCode == Activity.RESULT_OK)
+        {
+            if (filePath == null) return
+            filePath!!.onReceiveValue(WebChromeClient.FileChooserParams
+                                                     .parseResult(resultCode, data))
+            filePath = null
+        }
     }
 }
